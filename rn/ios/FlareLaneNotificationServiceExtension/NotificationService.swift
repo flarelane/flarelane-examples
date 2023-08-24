253ac97d39f728c1960cc565a2de98cc438fdfec
MinHyeok Kim <minhyeok4dev@gmail.com>
2023-08-24 12:12:18 +0900

    first commit

---
+++ rn/ios/FlareLaneNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,11 @@
+//
+//  NotificationService.swift
+//  FlareLaneNotificationServiceExtension
+//
+//  Created by MinHyeok Kim on 2023/08/24.
+//
+
+import UserNotifications
+import FlareLane
+
+class NotificationService: FlareLaneNotificationServiceExtension { }
