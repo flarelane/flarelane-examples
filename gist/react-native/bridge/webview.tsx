8def8415563ed86f1ed1235b6ec97071516c8b33
최준영 <62019543+junyeongCHOI@users.noreply.github.com>
2024-08-05 14:38:45 +0900

    Create webview.tsx

---
+++ gist/react-native/bridge/webview.tsx
@@ -0,0 +1,27 @@
+import FlareLane from '@flarelane/react-native-sdk';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <WebView
+          ...
+          onMessage={(event) => {
+            const data = JSON.parse(event.nativeEvent.data);
+            switch (data.type) {
+              case 'setUserId':
+                FlareLane.setState({ title: data.userId });
+                break;
+              case 'setTags':
+                FlareLane.setTags({ color: data.tags });
+                break;
+            }
+          }}
+        />
+      </View>
+    );
+  }
+}
